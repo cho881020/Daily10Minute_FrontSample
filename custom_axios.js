152a8507ec435cf903adc83f826d976edaefac8a
kj_cho <kj_cho@idealidea.co.kr>
2020-06-08 15:55:22 +0900

    메인화면에서 참여 가능 프로젝트 목록 호출
     - 커스텀Axios의 토큰 찾는 키를 변경

--- custom_axios.js
+++ custom_axios.js
@@ -2,5 +2,5 @@
 let axiosInstance = axios.create({
     baseURL: 'http://15.164.153.174',
     timeout: 3000,
-    headers: {'X-Http-Token': $.cookie('loginUserToken')}
+    headers: {'X-Http-Token': $.cookie('userToken')}
 })
