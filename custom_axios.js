33535a148f25853a966de98da72d78c3f1df58fe
조경진 <kj_cho@idealidea.co.kr>
2020-06-22 09:47:19 +0900

    인증글 조회시 프로젝트 정보도 내려오기

--- custom_axios.js
+++ custom_axios.js
@@ -1,6 +1,7 @@
 
 let axiosInstance = axios.create({
-    baseURL: 'http://15.164.153.174',
+    // baseURL: 'http://15.164.153.174',
+    baseURL: 'http://localhost:5000',
     timeout: 3000,
     headers: {'X-Http-Token': $.cookie('userToken')}
 })
