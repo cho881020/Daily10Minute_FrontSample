fa56489d02f7749cfb0d7f5ac2c59e93402aee4a
조경진 <kj_cho@idealidea.co.kr>
2020-06-27 15:14:46 +0900

    서버 주소 변경

--- custom_axios.js
+++ custom_axios.js
@@ -1,7 +1,7 @@
 
 let axiosInstance = axios.create({
-    // baseURL: 'http://15.164.153.174',
-    baseURL: 'http://localhost:5000',
+    baseURL: 'http://15.164.153.174',
+    // baseURL: 'http://localhost:5000',
     timeout: 3000,
     headers: {'X-Http-Token': $.cookie('userToken')}
 })
